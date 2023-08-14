ab2195008273e7b5576a1e5c84dc466c08a81ab1
Javi25214 <javitoyrosita@gmail.com>
2023-08-14 01:10:41 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -20,6 +20,5 @@ def delete_todo(position):
     del todos[position]
     return jsonify(todos), 200
 
-# These two lines should always be at the end of your app.py file.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
